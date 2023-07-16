6ad85d7fa4e2a967705931f603556026ca6196c5
aaka12345 <139609624+aaka12345@users.noreply.github.com>
2023-07-16 11:58:57 +0530

    add index.html and all file

---
+++ airbord.js
@@ -0,0 +1,17 @@
+function show(){
+    var a=document.getElementById("nav");
+    a.style.transform="translateX(0px)";
+    var b=document.getElementById("sqar");
+    b.style.display="none";
+    var c=document.getElementById("X");
+    c.style.display="block";
+    }
+    function hide(){
+    var a=document.getElementById("nav");
+    a.style.transform="translateX(-250px)";
+    var b=document.getElementById("X");
+    b.style.display="none";
+    var c=document.getElementById("sqar");
+    c.style.display="block";
+    }
+    
